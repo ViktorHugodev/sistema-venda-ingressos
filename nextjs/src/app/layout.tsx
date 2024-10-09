8d6ad7dc50c6363bbcc720b48db9ef3a47aa423a
viktorhugodev <viktorhugo.dev@gmail.com>
2024-10-09 09:21:34 -0300

    feat: add initial nextjs project, with custom themes

---
+++ nextjs/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+export const metadata: Metadata = {
+  title: 'Venda de Ingressos',
+  description: 'Sistema para venda de ingressos.',
+}
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${inter.className} flex flex-col min-h-screen items-center bg-primary text-default`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
