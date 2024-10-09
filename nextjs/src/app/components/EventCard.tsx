0bf694028131a06dae180d80f56ece3e8dca5963
viktorhugodev <viktorhugo.dev@gmail.com>
2024-10-09 10:01:35 -0300

    feat:add EventCard, EventImage, Navbar components, and models.

--- nextjs/src/app/components/EventCard.tsx
+++ nextjs/src/app/components/EventCard.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link'
-
-import Image from 'next/image'
 import { EventImage } from './EventImage'
+import { EventModel } from '@/models'
 
 export type EventCardProps = {
   event: EventModel
